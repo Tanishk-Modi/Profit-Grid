083c046f56b7dd75974d86c60cba85d8a9bc97ed
Tanishk <tanishkmodi9@gmail.com>
2025-07-08 16:21:51 -0400

    prepare for render deployment

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from api.endpoints import stocks
 from app.database import create_db_and_tables
 from api.endpoints import users
 from api.endpoints import watchlists
+import os
 
 app = FastAPI()
 
@@ -37,4 +38,5 @@ app.include_router(users.router, prefix="/api/v1/users")
 app.include_router(watchlists.router, prefix="/api/v1/watchlists")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
